196b7b4d3f18c7bb9fd3599e7ad8b1d73549d4da
smith109 <devon.smith017@gmail.com>
2024-08-14 20:44:58 -0500

    Create a function to perform an operation on the storage array

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
+const storage = [];
+let operator = '';
+
 const operations = {
   add: (a, b) => a + b,
   subtract: (a, b) => a - b,
   multiply: (a, b) => a * b,
   divide: (a, b) => b === 0 ? 'Error' : a / b
 };
+
+function operate(storage, operator) {
+  console.log(storage.reduce(operations[operator]));
+}
