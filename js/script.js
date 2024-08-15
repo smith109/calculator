01ad34466832d13ef0e12664dd2c1fe56063a69f
smith109 <devon.smith017@gmail.com>
2024-08-14 21:29:23 -0500

    Create an event handler for button clicks

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const calcDisplay = document.querySelector('.display');
+const calcButtons = document.querySelector('.btn-container');
 const storage = [];
 let operator = '';
+
+calcButtons.addEventListener('click', handleButtonClick);
 
 const operations = {
   add: (a, b) => a + b,
@@ -15,4 +18,21 @@ function operate(storage, operator) {
 
 function updateDisplay(displayValue) {
   calcDisplay.textContent = displayValue;
+}
+
+function handleButtonClick(event) {
+  const target = event.target;
+  const classList = target.classList;
+
+  switch (true) {
+    case classList.contains('number'):
+      console.log(`${target.textContent} was clicked`);
+      break;
+    case classList.contains('operator'):
+      console.log(`${target.textContent} was clicked`);
+      break;
+    case classList.contains('equals'):
+      console.log(`${target.textContent} was clicked`);
+      break;
+  }
 }
