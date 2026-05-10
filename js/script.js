f00595e84ba0b2d34278424e7d30a5d320620b5f
smith109 <devon.smith017@gmail.com>
2026-05-09 23:39:53 -0500

    Add logic to perform an arithmetic operation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
+let previousOperand = '';
+let currentOperand = '0';
+let operator = null;
+
 const operations = {
   add: (a, b) => a + b,
   subtract: (a, b) => a - b,
   multiply: (a, b) => a * b,
   divide: (a, b) => b === 0 ? 'Error' : a / b,
 };
+
+function operate(operator, previousOperand, currentOperand) {
+  const operationFunc = operations[operator];
+  return operationFunc(previousOperand, currentOperand);
+}
